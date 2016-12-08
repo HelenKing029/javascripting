7f7275791941dfba3cf955e68744d708ecceb668
HelenKing029 <helenking029@gmail.com>
2016-12-08 05:35:52 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(firstArg, secArg, thirdArg){
+  return  firstArg = secArg * thirdArg + firstArg
+};
+
+console.log(math(53, 61, 67));
---
+++ functions.js
@@ -0,0 +1,6 @@
+function eat (string){
+  food = 'bananas';
+  return food + ' tasted really good.';
+};
+
+console.log(eat());
